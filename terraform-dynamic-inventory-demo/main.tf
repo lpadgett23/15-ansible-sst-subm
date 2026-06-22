5682fe913c017451d6f5bdec3fc17e305ad16b8c
Laura <lpadgett23@gmail.com>
2026-06-22 17:14:14 -0400

    Moved ansible roles demo here

--- terraform-dynamic-inventory-demo/main.tf
+++ terraform-dynamic-inventory-demo/main.tf
@@ -127,13 +127,13 @@ resource "aws_instance" "myapp-server-two" {
   ami           = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
 
-  subnet_id              = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
   availability_zone      = var.avail_zone
 
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key.key_name
 
+  subnet_id              = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
   tags = {
     Name : "${var.env_prefix}-server"
   }
