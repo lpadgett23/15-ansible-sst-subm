e81f044465617ed2e78584014357145fcfc486da
Laura <lpadgett23@gmail.com>
2026-06-22 16:21:07 -0400

    troubleshooting server prep script addition

--- java-maven-app/prepare-ansible-server.sh
+++ java-maven-app/prepare-ansible-server.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt install ansible -y
-apt install python-boto3
+apt install python3-boto3
 
 # if i save aws credentials inside of Jenkins credentials as jenkins_aws_access_key_id and jenkins_aws_secret_access_key 
 # then... can automate this too
